e12335a01a7fdddeb12e4a7ba2bfab193327168c
Andrey Grishin <grishin@tiburon-research.ru>
2016-02-09 13:41:41 +0300

    осталось заполнить toasts.txt

--- bot.go
+++ bot.go
@@ -3,17 +3,29 @@ package main
 import "time"
 import "github.com/tucnak/telebot"
 import "os"
+import "io/ioutil"
+import "strings"
+import "math/rand"
+
+type toast struct {
+    Text string
+}
 
 func main() {
-	telegramApiKey := os.Getenv("DJIGURBOT_TELEGRAMAPIKEY")
-	if telegramApiKey == "" {
+	telegramAPIKey := os.Getenv("DJIGURBOT_TELEGRAMAPIKEY")
+	if telegramAPIKey == "" {
 		panic("Telegram api key is not set.")
 	}
 
-	bot, err := telebot.NewBot(telegramApiKey)
+	bot, err := telebot.NewBot(telegramAPIKey)
 	if err != nil {
 		panic("Could not connect to Telegram API")
 	}
+    
+    toasts, err := readToastsFile()
+    if err != nil {
+        panic("Could not read toasts.txt file")
+    }
 
 	messages := make(chan telebot.Message)
 	bot.Listen(messages, 1*time.Second)
@@ -22,6 +34,25 @@ func main() {
 		if message.Text == "/hi" {
 			bot.SendMessage(message.Chat,
 				"Привет, "+message.Sender.FirstName+"!", nil)
-		}
+		} else if message.Text == "Тост!" {
+            randomToast := toasts[rand.Intn(len(toasts))]            
+            bot.SendMessage(message.Chat, randomToast.Text, nil)
+        }
 	}
+}
+
+func readToastsFile() ([]toast, error) {
+    content, err := ioutil.ReadFile("toasts.txt")
+    if err != nil {
+       return nil, err
+    }
+    lines := strings.Split(string(content), "---")
+    
+    toasts := make([]toast, len(lines)) 
+    for i, line := range lines {
+         t := toast {Text : line }
+         toasts[i] = t
+    }
+    
+    return toasts, err
 }
