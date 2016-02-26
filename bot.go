3a1a5954f9d259418b0bb7305881cc41c5993942
Andrey Grishin <grishin@tiburon-research.ru>
2016-02-26 13:20:21 +0300

    Фразы для Артемия

--- bot.go
+++ bot.go
@@ -1,14 +1,17 @@
 package main
 
-import "time"
-import "github.com/tucnak/telebot"
-import "os"
-import "io/ioutil"
-import "strings"
-import "math/rand"
+import (
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/tucnak/telebot"
+)
 
 type toast struct {
-    Text string
+	Text string
 }
 
 func main() {
@@ -21,11 +24,11 @@ func main() {
 	if err != nil {
 		panic("Could not connect to Telegram API")
 	}
-    
-    toasts, err := readToastsFile()
-    if err != nil {
-        panic("Could not read toasts.txt file")
-    }
+
+	toasts, err := readToastsFile()
+	if err != nil {
+		panic("Could not read toasts.txt file")
+	}
 
 	messages := make(chan telebot.Message)
 	bot.Listen(messages, 1*time.Second)
@@ -34,25 +37,31 @@ func main() {
 		if message.Text == "/hi" {
 			bot.SendMessage(message.Chat,
 				"Привет, "+message.Sender.FirstName+"!", nil)
-		} else if message.Text == "Тост!" {
-            randomToast := toasts[rand.Intn(len(toasts))]            
-            bot.SendMessage(message.Chat, randomToast.Text, nil)
-        }
+		} else if strings.Contains(message.Text, "Тост!") {
+			randomToast := toasts[rand.Intn(len(toasts))]
+			bot.SendMessage(message.Chat, randomToast.Text, nil)
+		} else if message.Text == "Костя!" {
+			bot.SendMessage(message.Chat, "Костя занят, он на речном!", nil)
+		} else if strings.Contains(message.Text, "Твоя!") {
+			bot.SendMessage(message.Chat, "Нееееет, твоя!", nil)
+		} else if strings.Contains(message.Text, "крутой") {
+			bot.SendMessage(message.Chat, "Нееееет, "+message.Sender.FirstName+", это ты крутой!", nil)
+		}
 	}
 }
 
 func readToastsFile() ([]toast, error) {
-    content, err := ioutil.ReadFile("toasts.txt")
-    if err != nil {
-       return nil, err
-    }
-    lines := strings.Split(string(content), "---")
-    
-    toasts := make([]toast, len(lines)) 
-    for i, line := range lines {
-         t := toast {Text : line }
-         toasts[i] = t
-    }
-    
-    return toasts, err
+	content, err := ioutil.ReadFile("toasts.txt")
+	if err != nil {
+		return nil, err
+	}
+	lines := strings.Split(string(content), "---")
+
+	toasts := make([]toast, len(lines))
+	for i, line := range lines {
+		t := toast{Text: line}
+		toasts[i] = t
+	}
+
+	return toasts, err
 }
